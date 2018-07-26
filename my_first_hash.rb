c262bf09dce1c597ea86c94e1bca85c975517932
Koy Saeteurn <kosatun@aol.com>
2018-07-26 23:16:32 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,6 +29,6 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2 
-  shipping_manifest["gunpowder"] = 4
+  shipping_manifest["gun powder"] = 4
   shipping_manifest
 end
